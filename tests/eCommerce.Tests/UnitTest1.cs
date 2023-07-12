43f036420e1762ab85f2b793e9b477e914d0d5dd
aksoymustafacn <aksoy.mustafacn@gmail.com>
2023-07-12 17:44:44 +0300

    created test project

---
+++ tests/eCommerce.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace eCommerce.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
