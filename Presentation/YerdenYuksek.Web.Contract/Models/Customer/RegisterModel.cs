cc3c002ee3ed7505028e6578f00e34702cf8b251
aksoymustafacn <aksoy.mustafacn@gmail.com>
2023-07-12 17:16:07 +0300

    updated project structure

--- Presentation/YerdenYuksek.Web.Contract/Models/Customer/RegisterModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace YerdenYuksek.Web.Contract.Models.Customer;
-
-public record RegisterModel : BaseModel
-{
-    public string Email { get; set; }
-
-    public string Password { get; set; }
-}
